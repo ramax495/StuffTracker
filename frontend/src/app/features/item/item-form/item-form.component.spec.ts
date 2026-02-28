82a525e3d267b9d57080401d501f2b0d8b80e974
Maxim Stories <ramax495>
2026-02-28 14:54:28 +0300

    fix(frontend): remove unused fakeAsync/tick imports in item-form spec

--- frontend/src/app/features/item/item-form/item-form.component.spec.ts
+++ frontend/src/app/features/item/item-form/item-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { of, throwError } from 'rxjs';
